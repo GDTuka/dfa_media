b9791d92c8509e798618841fe3fe80fd3fd4201d
tuka <artem.soglaev@mail.ru>
2024-02-05 12:57:54 +0300

    add new prints

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
+  print('asdf;jlakl;sdfjl');
+  print('a;sjdfk;asjdfl');
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(
